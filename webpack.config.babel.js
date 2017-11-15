6097973e851a0ca5b73bcec8f09186be60cb0f8e
Willian R. Sabião <willianjedai@gmail.com>
2017-11-15 00:31:43 +0000

    feat: image optimizator

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -45,6 +45,36 @@ const webpackConfig = {
           fallback: 'style-loader',
           use: ['css-loader', 'stylus-loader'] 
         }),
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
       }
     ],
   },
